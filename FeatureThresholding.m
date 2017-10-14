8920e1ac02321673bc7edd1fbf6d1eeae2808c85
julielaurent <32266931+julielaurent@users.noreply.github.com>
2017-10-14 18:35:05 +0200

    juju

--- FeatureThresholding.m
+++ FeatureThresholding.m
@@ -12,12 +12,13 @@ y = features(find(labels == 0), :); %vector of class B
 %% Threshold choice with histograms ???
 % % We take good features : 362, 366
 % % Histograms can help us find the TH how ?????
-% figure('Color','w');
-% histogram(x(:,362));
-% hold on;
-% histogram(y(:,362));
-% box off;
-% hold off;
+figure('Color','w');
+histogram(x(:,362));
+hold on;
+histogram(y(:,362));
+legend('Correct movement of Cursor','Erroneous Movement of Cursor');
+box off;
+hold off;
 % figure('Color','w');
 % histogram(x(:,366));
 % hold on;
@@ -25,38 +26,117 @@ y = features(find(labels == 0), :); %vector of class B
 % box off;
 % hold off;
 
-%% Scatter
+%% Classification and optimal TH
+errorClassification362 = zeros(134,1);
+errorClass362 = zeros(134,1);
+
+sf362 = features(:,362);
+it = 0;
+for tf362 = -6:0.09:6
+    
+    nCorrect362 = 0;
+    misClassA = 0;
+    misClassB = 0;
+    it = it + 1;
+    
+    labelf362 = (sf362 > tf362); % element is 1 if Class A
+    
+    for i = 1:648
+        if (labels(i) == labelf362(i))
+            nCorrect362 = nCorrect362 + 1;
+
+        else
+            if (labels(i) == 1)
+                misClassA = misClassA + 1;
+            else
+                misClassB = misClassB + 1;
+            end
+        end
+    end
+    
+errorClassification362(it) = 1 - nCorrect362/648;
+errorClass362(it) = 0.5*misClassA/516 + 0.5*misClassB/132;
+
+end
+
 figure('Color','w');
-%tf362 = min;
-%tf366 = ;
-scatter(features(:,362),features(:,366),'.');
+th = -6:0.09:6;
+plot(th,errorClassification362, th, errorClass362);
+xlabel('Threshold'); ylabel('Error');
+legend('Classification Error','Class Error');
+box off;
+
+optimalTH1 = th(find(errorClassification362 == min(errorClassification362)));
+optimalTH2 = th(find(errorClass362 == min(errorClass362)));
+
+%% Scatter
+% We keep feature 362
+figure('Color','w');
+scatter(x(:,362),x(:,366),'.b');
+hold on;
+scatter(y(:,362),y(:,366),'.r');
 xlabel('Feature 362'); ylabel('Feature 366');
 hold on;
-%ine(tf362);
+vline(optimalTH1,'k--');
+hold on;
+vline(optimalTH2,'k-.');
+legend('Samples from Class A','Samples from Class B');
 hold off;
 
-%% Classification
-% sf362 = features(:,362);
-% % tf362 = ;
-% sf366 = features(:,366);
-% % tf366 = ;
-% labelf362 = (sf362 < tf362); % element is 1 if ClassA
-% labelf366 = (sf366 < tf366); % element is 1 if ClassA
-% 
-% %% Error
-% nCorrect362 = 0;
-% nCorrect366 = 0;
-% for i = 1:size(labels)
-%     if (labels(i) == labelf362(i))
-%         nCorrect362 = nCorrect362 + 1;
-%     end
-%     if (labels(i) == labelf366(i))
-%         nCorrect362 = nCorrect366 + 1;
-%     end
-% end
-% errorClassification362 = 1 - nCorrect362/size(labels);
-% errorClassification366 = 1 - nCorrect366/size(labels);
-% errorClass = ;
-% 
-% %% Plot error classification
-% th = -4:0.5:6;
+%% Scale to two features
+
+errorClassification = zeros(121,1);
+errorClass = zeros(121,1);
+labelsDouble = [labels;labels];
+
+sf = [features(:,362);features(:,366)];
+
+it = 0;
+for tf = -6:0.1:6
+    
+    nCorrect = 0;
+    misClassA = 0;
+    misClassB = 0;
+    it = it + 1;
+    
+    labelf = (sf > tf); % element is 1 if Class A
+    
+    for i = 1:2*648
+        if (labelsDouble(i) == labelf(i))
+            nCorrect = nCorrect + 1;
+
+        else
+            if (labelsDouble(i) == 1)
+                misClassA = misClassA + 1;
+            else
+                misClassB = misClassB + 1;
+            end
+        end
+    end
+    
+errorClassification(it) = 1 - nCorrect/(2*648);
+errorClass(it) = 0.5*misClassA/(2*516) + 0.5*misClassB/(2*132);
+
+end
+
+figure('Color','w');
+th = -6:0.1:6;
+plot(th,errorClassification, th, errorClass);
+xlabel('Threshold'); ylabel('Error');
+legend('Classification Error','Class Error');
+box off;
+
+optimalTH1Double = th(find(errorClassification == min(errorClassification)));
+optimalTH2Double = th(find(errorClass == min(errorClass)));
+
+figure('Color','w');
+scatter(x(:,362),x(:,366),'.b');
+hold on;
+scatter(y(:,362),y(:,366),'.r');
+xlabel('Feature 362'); ylabel('Feature 366');
+hold on;
+vline(optimalTH1Double,'k--');
+hold on;
+vline(optimalTH2Double,'k-.');
+legend('Samples from Class A','Samples from Class B');
+hold off;
