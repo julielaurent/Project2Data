96d3e5e98c7c3c401bc5c6b825e30bd1ec7aba70
Alex Sebastiani <alexsebastiani@tsf-436-wpa-5-120.epfl.ch>
2017-10-26 13:25:35 +0200

    Code 2ème partie mis au propre pour rendu

--- Part2/CrossValidation.m
+++ Part2/CrossValidation.m
@@ -15,141 +15,138 @@ size_labels = size(labels);
 % iteration in order to one time use the 1 testing, and 2,3,4.. training,
 % then the 2 testing, and 1,3,4,5... training etc...
 
-% N = size_labels(1); % N = total number of samples
-% cp_N = cvpartition(N,'kfold',10); % Outputs: Trainsize and Testsize contain the size (= number of samples) of each train/test set
-% cp_labels = cvpartition (labels,'kfold',10);
-% 
-% % Initialization of error vectors
-% errClassLin = zeros(cp_labels.NumTestSets,1);
-% errClassDiagLin = zeros(cp_labels.NumTestSets,1);
-% errClassDiagQuadr = zeros(cp_labels.NumTestSets,1);
-% ratiosTrain = zeros(cp_labels.NumTestSets,1);
-% ratiosTest = zeros(cp_labels.NumTestSets,1);
-% 
-% % For a different testSet i each time
-% for i = 1:cp_labels.NumTestSets
-%     
-%     % Attention,ici le cp_N.taining rend les INDICES des train samples
-%     % Quand trainIdx = 1 -> sample qui va dans le trainSet
-%     trainIdx = cp_labels.training(i);
-%     trainSet = features(trainIdx,:);
-%     trainLabels = labels(trainIdx);
-%    
-%     % Attention, ici le cp_N.test rend les INDICES des test samples
-%     % Quand testIdx = 1 -> sample va dans le testSet
-%     testIdx = cp_labels.test(i);
-%     testSet = features(testIdx,:);
-%     testLabels = labels(testIdx);
-%     
-%     % Ratios
-%     ratiosTrain(i) = size(find(trainLabels == 1))/size(find(trainLabels == 0));
-%     ratiosTest(i) = size(find(testLabels == 1))/size(find(testLabels == 0));
-%     
-%     % Calculus of class errors
-%     Linclassifier = fitcdiscr(trainSet,trainLabels,'discrimtype', 'linear');
-%     Lin_y = predict(Linclassifier,testSet);
-%     errClassLin(i) = classerrorOriginal(testLabels, Lin_y);
-%     
-%     DiagLinclassifier = fitcdiscr(trainSet,trainLabels,'discrimtype', 'diagLinear');
-%     DiagLin_y = predict(DiagLinclassifier,testSet);
-%     errClassDiagLin(i) = classerrorOriginal(testLabels, DiagLin_y);
-%    
-%     DiagQuadrclassifier = fitcdiscr(trainSet,trainLabels,'discrimtype', 'diagQuadratic');
-%     DiagQuadr_y = predict(DiagQuadrclassifier,testSet);
-%     errClassDiagQuadr(i) = classerrorOriginal(testLabels, DiagQuadr_y);
-%     
-% end
-% 
-% % % Mean of errors
-% cvErrLin = mean(errClassLin);
-% cvErrDiagLin = mean(errClassDiagLin);
-% cvErrDiagQuadr = mean(errClassDiagQuadr);
-% 
-% % Standard deviations of errors
-% cvStdLin = std(errClassLin);
-% cvStdDiagLin = std(errClassDiagLin);
-% cvStdDiagQuadr = std(errClassDiagQuadr);
-% 
-% % Mean and std of ratios
-% rMTrain = mean(ratiosTrain);
-% rMTest = mean(ratiosTest);
-% rSTDTrain = std(ratiosTrain);
-% rSTDTest = std(ratiosTest);
-% 
-% 
-% % Plot
-% figure('Color','w');
-% title('10-fold Cross Validation Error');
-% hold on;
-% barwitherr([cvStdDiagLin ; cvStdLin ; cvStdDiagQuadr], [cvErrDiagLin ; cvErrLin ; cvErrDiagQuadr]);
-% ylabel('Class Error');
-% xticks([1 2 3]);
-% xticklabels({'DiagLinear','Linear','DiagQuadratic'});
-% axis([0.5 3.5 0 0.7]);
-% box off;
-% hold off;
-% 
-% % Leave One Out Cross Validation
-% 
-% C = cvpartition(labels,'LeaveOut'); %"leave-one-out" cross validation
-% % Error Calculation
-% errClassLinC = zeros(C.NumTestSets,1);
-% errClassDiagLinC = zeros(C.NumTestSets,1);
-% errClassDiagQuadrC = zeros(C.NumTestSets,1);
-% 
-% for i = 1:C.NumTestSets
-%     
-%     % A rajouter pour derniere question
-%     %C = repartition(C);
-%     
-%     % Attention,ici le cp_N.taining rend les INDICES des train samples
-%     % Quand trainIdx = 1 -> sample qui va dans le trainSet
-%     trainIdxC = C.training(i);
-%     trainSetC = features(trainIdxC,:);
-%     trainLabelsC = labels(trainIdxC);
-%     
-%     % Attention, ici le cp_N.test rend les INDICES des test samples
-%     % Quand testIdx = 1 -> sample va dans le testSet
-%     testIdxC = C.test(i);
-%     testSetC = features(testIdxC,:);
-%     testLabelsC = labels(testIdxC);
-%     
-%     % Calculus of class errors
-%     LinclassifierC = fitcdiscr(trainSetC,trainLabelsC,'discrimtype', 'linear');
-%     Lin_yC = predict(LinclassifierC,testSetC);
-%     errClassLinC(i) = classerrorOriginal(testLabelsC, Lin_yC);
-%     
-%     DiagLinclassifierC = fitcdiscr(trainSetC,trainLabelsC,'discrimtype', 'diagLinear');
-%     DiagLin_yC = predict(DiagLinclassifierC,testSetC);
-%     errClassDiagLinC(i) = classerrorOriginal(testLabelsC, DiagLin_yC);
-%     
-%     DiagQuadrclassifierC = fitcdiscr(trainSetC,trainLabelsC,'discrimtype', 'diagQuadratic');
-%     DiagQuadr_yC = predict(DiagQuadrclassifierC,testSetC);
-%     errClassDiagQuadrC(i) = classerrorOriginal(testLabelsC, DiagQuadr_yC);
-%     
-% end
-% 
-% % Mean of errors 
-% cvErrLinC = mean(errClassLinC);
-% cvErrDiagLinC = mean(errClassDiagLinC);
-% cvErrDiagQuadrC = mean(errClassDiagQuadrC);
-% 
-% % Standard deviations of errors
-% cvStdLinC = std(errClassLinC);
-% cvStdDiagLinC = std(errClassDiagLinC);
-% cvStdDiagQuadrC = std(errClassDiagQuadrC);
-% 
-% % Plot
-% figure('Color','w');
-% title('Leave One Out Cross Validation Error');
-% hold on;
-% barwitherr([cvStdDiagLinC ; cvStdLinC ; cvStdDiagQuadrC], [cvErrDiagLinC ; cvErrLinC ; cvErrDiagQuadrC]);
-% ylabel('Class Error');
-% xticks([1 2 3]);
-% xticklabels({'DiagLinear','Linear','DiagQuadratic'});
-% axis([0.5 3.5 -0.3 0.7]);
-% box off;
-% hold off;
+N = size_labels(1); % N = total number of samples
+cp_N = cvpartition(N,'kfold',10); % Outputs: Trainsize and Testsize contain the size (= number of samples) of each train/test set
+cp_labels = cvpartition (labels,'kfold',10);
+
+% Initialization of error vectors
+errClassLin = zeros(cp_labels.NumTestSets,1);
+errClassDiagLin = zeros(cp_labels.NumTestSets,1);
+errClassDiagQuadr = zeros(cp_labels.NumTestSets,1);
+ratiosTrain = zeros(cp_labels.NumTestSets,1);
+ratiosTest = zeros(cp_labels.NumTestSets,1);
+
+% For a different testSet i each time
+for i = 1:cp_labels.NumTestSets
+    
+    % Attention,ici le cp_N.taining rend les INDICES des train samples
+    % Quand trainIdx = 1 -> sample qui va dans le trainSet
+    trainIdx = cp_labels.training(i);
+    trainSet = features(trainIdx,:);
+    trainLabels = labels(trainIdx);
+   
+    % Attention, ici le cp_N.test rend les INDICES des test samples
+    % Quand testIdx = 1 -> sample va dans le testSet
+    testIdx = cp_labels.test(i);
+    testSet = features(testIdx,:);
+    testLabels = labels(testIdx);
+    
+    % Ratios
+    ratiosTrain(i) = size(find(trainLabels == 1))/size(find(trainLabels == 0));
+    ratiosTest(i) = size(find(testLabels == 1))/size(find(testLabels == 0));
+    
+    % Calculus of class errors
+    Linclassifier = fitcdiscr(trainSet,trainLabels,'discrimtype', 'linear');
+    Lin_y = predict(Linclassifier,testSet);
+    errClassLin(i) = classerrorOriginal(testLabels, Lin_y);
+    
+    DiagLinclassifier = fitcdiscr(trainSet,trainLabels,'discrimtype', 'diagLinear');
+    DiagLin_y = predict(DiagLinclassifier,testSet);
+    errClassDiagLin(i) = classerrorOriginal(testLabels, DiagLin_y);
+   
+    DiagQuadrclassifier = fitcdiscr(trainSet,trainLabels,'discrimtype', 'diagQuadratic');
+    DiagQuadr_y = predict(DiagQuadrclassifier,testSet);
+    errClassDiagQuadr(i) = classerrorOriginal(testLabels, DiagQuadr_y);
+    
+end
+
+% % Mean of errors
+cvErrLin = mean(errClassLin);
+cvErrDiagLin = mean(errClassDiagLin);
+cvErrDiagQuadr = mean(errClassDiagQuadr);
+
+% Standard deviations of errors
+cvStdLin = std(errClassLin);
+cvStdDiagLin = std(errClassDiagLin);
+cvStdDiagQuadr = std(errClassDiagQuadr);
+
+% Mean and std of ratios
+rMTrain = mean(ratiosTrain);
+rMTest = mean(ratiosTest);
+rSTDTrain = std(ratiosTrain);
+rSTDTest = std(ratiosTest);
+
+
+% Plot
+figure('Color','w');
+title('10-fold Cross Validation Error');
+hold on;
+barwitherr([cvStdDiagLin ; cvStdLin ; cvStdDiagQuadr], [cvErrDiagLin ; cvErrLin ; cvErrDiagQuadr]);
+ylabel('Class Error');
+xticks([1 2 3]);
+xticklabels({'DiagLinear','Linear','DiagQuadratic'});
+axis([0.5 3.5 0 0.7]);
+box off;
+hold off;
+
+% Leave One Out Cross Validation
+
+C = cvpartition(labels,'LeaveOut'); %"leave-one-out" cross validation
+% Error Calculation
+errClassLinC = zeros(C.NumTestSets,1);
+errClassDiagLinC = zeros(C.NumTestSets,1);
+errClassDiagQuadrC = zeros(C.NumTestSets,1);
+
+for i = 1:C.NumTestSets
+    
+    % Attention,ici le cp_N.taining rend les INDICES des train samples
+    % Quand trainIdx = 1 -> sample qui va dans le trainSet
+    trainIdxC = C.training(i);
+    trainSetC = features(trainIdxC,:);
+    trainLabelsC = labels(trainIdxC);
+    
+    % Attention, ici le cp_N.test rend les INDICES des test samples
+    % Quand testIdx = 1 -> sample va dans le testSet
+    testIdxC = C.test(i);
+    testSetC = features(testIdxC,:);
+    testLabelsC = labels(testIdxC);
+    
+    % Calculus of class errors
+    LinclassifierC = fitcdiscr(trainSetC,trainLabelsC,'discrimtype', 'linear');
+    Lin_yC = predict(LinclassifierC,testSetC);
+    errClassLinC(i) = classerrorOriginal(testLabelsC, Lin_yC);
+    
+    DiagLinclassifierC = fitcdiscr(trainSetC,trainLabelsC,'discrimtype', 'diagLinear');
+    DiagLin_yC = predict(DiagLinclassifierC,testSetC);
+    errClassDiagLinC(i) = classerrorOriginal(testLabelsC, DiagLin_yC);
+    
+    DiagQuadrclassifierC = fitcdiscr(trainSetC,trainLabelsC,'discrimtype', 'diagQuadratic');
+    DiagQuadr_yC = predict(DiagQuadrclassifierC,testSetC);
+    errClassDiagQuadrC(i) = classerrorOriginal(testLabelsC, DiagQuadr_yC);
+    
+end
+
+% Mean of errors 
+cvErrLinC = mean(errClassLinC);
+cvErrDiagLinC = mean(errClassDiagLinC);
+cvErrDiagQuadrC = mean(errClassDiagQuadrC);
+
+% Standard deviations of errors
+cvStdLinC = std(errClassLinC);
+cvStdDiagLinC = std(errClassDiagLinC);
+cvStdDiagQuadrC = std(errClassDiagQuadrC);
+
+% Plot
+figure('Color','w');
+title('Leave One Out Cross Validation Error');
+hold on;
+barwitherr([cvStdDiagLinC ; cvStdLinC ; cvStdDiagQuadrC], [cvErrDiagLinC ; cvErrLinC ; cvErrDiagQuadrC]);
+ylabel('Class Error');
+xticks([1 2 3]);
+xticklabels({'DiagLinear','Linear','DiagQuadratic'});
+axis([0.5 3.5 -0.3 0.7]);
+box off;
+hold off;
 
 %% Different repartitions
 
@@ -192,7 +189,6 @@ end
 figure('Color','w');
 title('10-fold cross Validation Error (changing repartition)');
 hold on;
-%barwitherr([cvStdLin(1) ; cvStdLin(2) ; cvStdLin(3) ; cvStdLin(4)], [cvErrLin(1) ; cvErrLin(2) ; cvErrLin(3) ; cvErrLin(4)]);
 barwitherr(cvStdDiagQuadr, cvErrDiagQuadr);
 ylabel('Class Error');
 xticks([1 2 3 4]);
@@ -239,7 +235,6 @@ end
 figure('Color','w');
 title('Leave-one out Cross Validation Error (changing repartition)');
 hold on;
-%barwitherr([cvStdLinC(1) ; cvStdLinC(2) ; cvStdLinC(3) ; cvStdLinC(4)], [cvErrLinC(1) ; cvErrLinC(2) ; cvErrLinC(3) ; cvErrLinC(4)]);
 barwitherr(cvStdDiagQuadrC, cvErrDiagQuadrC);
 ylabel('Class Error');
 xticks([1 2 3 4]);
